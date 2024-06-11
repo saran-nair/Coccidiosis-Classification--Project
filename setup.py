9502532fdc3d8bc48a120dda1c9401437eb71d06
Saran_Nair <saransomannair@gmail.com>
2024-06-11 11:20:40 +0200

    Updated

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__="0.0.0"
-REPO_NAME = "Coccidiosis-Classification--Project"
-AUTHOR_USER_NAME = "saran-nair"
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Chicken-Disease-Classification--Project"
+AUTHOR_USER_NAME = "entbappy"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "saransomannair@gmail.com"
+AUTHOR_EMAIL = "entbappy73@gmail.com"
+
 
 setuptools.setup(
     name=SRC_REPO,
@@ -15,8 +18,8 @@ setuptools.setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package for CNN app",
-    Long_description=long_description,
-    Long_description_content="text/markdown",
+    long_description=long_description,
+    long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
--- template.py
+++ template.py
@@ -1,8 +1,9 @@
 import os
-from pathlib import Path 
+from pathlib import Path
 import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
 
 project_name = "cnnClassifier"
 
@@ -23,18 +24,25 @@ list_of_files = [
     "setup.py",
     "research/trials.ipynb",
     "templates/index.html"
+
+
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
+
     if filedir !="":
-        os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating Directory; {filedir} for the file: {filename}")
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
-            logging.info(f"Creating Empty file: {filepath}")
+            logging.info(f"Creating empty file: {filepath}")
+
+
     else:
-        logging.info(f"{filename} already exists")
+        logging.info(f"{filename} is already exists")
